27ead338c47054e4ddd7e92f508ffb96e3cf7f6d
Candy S Chevalier <CandySueC@outlook.com>
2025-11-21 19:02:59 -0500

    fix: remove accidental .next folders and workspace overrides

--- apps/candyland/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2tcAkb4EPZhwxEJKQr+p30uNzoD7gblLpQnh1luyc4A=\"\n}"
--- apps/website/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"UlBkexGs112042oXNUoYJE6bjHMDNbsrmFqulhV7uW0=\"}"
